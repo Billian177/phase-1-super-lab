34551433e9f4d33e03f1482dd1b2e01cac456848
bill apiyo <billian177@gmail.com>
2022-06-23 14:30:24 +0300

    commit tree b634861672f6d3479256006819030a429206373d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your classes here
+//parent class tree
+class Tree {
+    constructor(species){
+        this.species= species;
+        this.name= null;
+    }
+
+    static definition(){
+        return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+    }
+}
+
+//child class
+class Deciduous extends Tree {
+    constructor (species,name){
+        super(species);
+        this.name= name;
+    }
+    static definition(){
+        return `${super.definition()} Deciduous trees shed their leaves annually.`
+    }
+}
+
+///child class
+class Evergreen extends Tree {
+    constructor(species,name){
+        super(species);
+        this.name= name;
+    }
+    static definition(){
+        return `${super.definition()} Evergreens keep their leaves all year round.`
+    }
+}
